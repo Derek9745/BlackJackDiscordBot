ebb5f2a3e31a9a8e47c2293128540d51447f9b96
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2024-07-07 15:07:46 -0400

    Add project files.

---
+++ BlackJackDiscordBot/Commands/BlackJackCommands.cs
@@ -0,0 +1,61 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace BlackJackDiscordBot.Commands
+{
+    internal class BlackJackCommands : BaseCommandModule
+    {
+        [Command("test")]
+
+        public async Task MyFirstCommand(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync($"Hello{ctx.User.Username}");
+        }
+
+        [Command("embed")]
+
+        public async Task EmbededMessage(CommandContext ctx)
+        {
+            var message = new DiscordEmbedBuilder
+            {
+                Title = "this is my first Discord Embed",
+                Description = $"This command was executed by {ctx.User.Username}",
+                Color = DiscordColor.Blue
+            };
+
+            await ctx.Channel.SendMessageAsync(embed:message);
+        }
+
+        [Command("PlayBlackJack")]
+
+        public async Task BlackJack(CommandContext ctx)
+        {
+            BlackJackSimulator blackJackSimulator = new BlackJackSimulator();
+       
+            var blackJackEmbed = new DiscordEmbedBuilder
+            {
+                Title ="Game Started!",
+                Description = $"Player hand Dealt.\n" +
+                $"{blackJackSimulator.gameSetup()}" +
+                $"\n Hands have been dealt. What would you like to do? Please enter a number value for a command" +
+                $"\n Current Deck size: {blackJackSimulator.getDeckSize()}",
+                Color = DiscordColor.Blue
+            };
+
+            await ctx.Channel.SendMessageAsync(embed: blackJackEmbed);
+
+           
+
+
+
+
+
+        }
+
+        
+    }
+}
---
+++ BlackJackDiscordBot/JSONReader.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackDiscordBot
+{
+    internal class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string Json = await sr.ReadToEndAsync();
+                JSONStructure data = JsonConvert.DeserializeObject<JSONStructure>(Json);
+
+                this.token = data.token;
+                this.prefix = data.prefix;
+            
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
